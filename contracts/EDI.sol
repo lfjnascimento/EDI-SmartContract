7b64a7c353cd251e56eec60549b883138a52777f
lfjnascimento <lfjnascimento@gmail.com>
2021-11-04 17:38:53 -0400

    Add more infos about degree

--- contracts/EDI.sol
+++ contracts/EDI.sol
@@ -5,17 +5,24 @@ pragma solidity ^0.8.0;
 struct Student{
   string name;
   string nationality;
-  string RG;
+  string RG_number;
+  string RG_agency;
+  string RG_UF;
+  string birthplace;
   uint256 birthdate;
 }
 
 struct Degree{
   bytes32 ID;
+  string number;
+  string register_number;
+  string process_number;
   string course;
   string academic_degree;
   uint256 completion_date;
   uint256 graduation_date;
   uint256 issue_date;
+  uint256 registration_date;
   bool is_valid;
   Student student;
 }
@@ -23,6 +30,7 @@ struct Degree{
 contract EDI{
   string public IES_name = unicode"Instituto Federal de Educação, Ciência e Tecnologia de Mato Grosso";
   string public IES_acronym = "IFMT";
+  string public IES_CNPJ = "00000000000";
   string[] public IES_courses;
   
   address  authority;
@@ -41,15 +49,15 @@ contract EDI{
   }
   
   modifier onlyIssuers{
-      bool is_issuer = false;
-      
-      for (uint16 i=0; i < issuers.length; i++){
-        if(msg.sender == issuers[i])
-          is_issuer = true;
-      }
-      
-      require(is_issuer, "only_issuers");
-      _;
+    bool is_issuer = false;
+    
+    for (uint16 i=0; i < issuers.length; i++){
+      if(msg.sender == issuers[i])
+        is_issuer = true;
+    }
+    
+    require(is_issuer, "only_issuers");
+    _;
   }
   
   function addIssuer(address _issuer) public onlyAuthority{
@@ -88,15 +96,22 @@ contract EDI{
   }
   
   function AddDegree(
+    string memory _number,
+    string memory _register_number,
+    string memory _process_number,
     string memory _course,
     string memory _academic_degree,
     uint256 _completion_date,
     uint256 _graduation_date,
     uint256 _issue_date,
+    uint256 _registration_date,
     string memory _student_name,
     string memory _student_nationality,
-    string memory _student_RG,
+    string memory _student_RG_number,
+    string memory _student_RG_agency,
+    string memory _student_RG_UF,
     string memory _student_CPF,
+    string memory _student_birthplace,
     uint256 _student_birthdate
   ) public onlyIssuers returns(bytes32 degreeID){
           
@@ -105,16 +120,23 @@ contract EDI{
     require(hasCourse(_course), "course_not_found");
     require(!hasDegree(ID), "degree_already_exists");
     
-    degrees[ID].ID = ID;
-    degrees[ID].course = _course;
-    degrees[ID].academic_degree = _academic_degree;
-    degrees[ID].completion_date = _completion_date;
-    degrees[ID].graduation_date = _graduation_date;
-    degrees[ID].issue_date = _issue_date;
-    degrees[ID].student.name = _student_name;
-    degrees[ID].student.nationality = _student_nationality;
-    degrees[ID].student.RG = _student_RG;
-    degrees[ID].student.birthdate = _student_birthdate;
+    degrees[ID].ID                    = ID;
+    degrees[ID].number                = _number;
+    degrees[ID].register_number       = _register_number;
+    degrees[ID].process_number        = _process_number;
+    degrees[ID].course                = _course;
+    degrees[ID].academic_degree       = _academic_degree;
+    degrees[ID].completion_date       = _completion_date;
+    degrees[ID].graduation_date       = _graduation_date;
+    degrees[ID].issue_date            = _issue_date;
+    degrees[ID].registration_date     = _registration_date;
+    degrees[ID].student.name          = _student_name;
+    degrees[ID].student.nationality   = _student_nationality;
+    degrees[ID].student.RG_number     = _student_RG_number;
+    degrees[ID].student.RG_agency     = _student_RG_agency;
+    degrees[ID].student.RG_UF         = _student_RG_UF;
+    degrees[ID].student.birthplace    = _student_birthplace;
+    degrees[ID].student.birthdate     = _student_birthdate;
     degrees[ID].is_valid = true;
     
     degrees_keys.push(ID);

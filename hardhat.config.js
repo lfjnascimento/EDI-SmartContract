5a492943f60d6d84f3f729ca737236d3d70ae14e
lfjnascimento <lfjnascimento@gmail.com>
2021-11-05 14:23:17 -0400

    Add Ropsten configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
@@ -18,4 +19,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    ropsten: {
+      chainId: 3,
+      url: process.env.ROPSTEN_URL,
+      accounts:
+      [
+         process.env.ROPSTEN_SENDER,
+      ]
+    }
+  },
 };

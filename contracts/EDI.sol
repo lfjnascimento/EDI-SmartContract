98d17547f694b1d3dee8401bb52eb3538aa397d9
lfjnascimento <lfjnascimento@gmail.com>
2021-11-06 03:03:03 -0400

    Using courses with struct and mapping

--- contracts/EDI.sol
+++ contracts/EDI.sol
@@ -12,32 +12,39 @@ struct Student{
   uint256 birthdate;
 }
 
+struct Course{
+  uint16 ID;
+  string name;
+  string academic_degree;
+}
+
 struct Degree{
   bytes32 ID;
   string number;
   string register_number;
   string process_number;
-  string course;
-  string academic_degree;
   uint256 completion_date;
   uint256 graduation_date;
   uint256 issue_date;
   uint256 registration_date;
-  bool is_valid;
+  uint16 course_ID;
   Student student;
+  bool is_valid;
 }
 
 contract EDI{
   string public IES_name = unicode"Instituto Federal de Educação, Ciência e Tecnologia de Mato Grosso";
   string public IES_acronym = "IFMT";
-  string public IES_CNPJ = "00000000000";
-  string[] public IES_courses;
+  string public IES_CNPJ = "11111111111111";
+
+  address public admin;      // Endereco do administrador do contrato.
+  address[] public issuers;  // Enderecos autorizados a emitir diplomas.
   
-  address  admin;
-  address[] issuers;
-  
-  mapping(bytes32 => Degree) degrees;
-  bytes32[] degrees_keys;
+  mapping(bytes32 => Degree) public degrees; // Mapping que armazena todos so diplomas.
+  bytes32[] degrees_keys;             // Lista das chaves dos diplomas.
+
+  mapping(uint16 => Course) public courses;  // Mapping que armazena todos os cursos ofertados pela IES.
+  uint16 private last_course_ID = 0;          // Ultimo valor utilizado como course_ID.
   
   constructor(){
     admin = msg.sender;
@@ -59,12 +66,44 @@ contract EDI{
     require(is_issuer, "only_issuers");
     _;
   }
+
+  /*
+  * Adiciona um novo curso a lista de cursos (courses) da IES.
+  */
+  function addCourse(string memory _name, string memory _academic_degree) public onlyAdmin{
+    uint16 ID = ++last_course_ID;
+
+    courses[ID].name = _name;
+    courses[ID].academic_degree = _academic_degree;
+  }
+
+  /*
+  * Altera as informacoes do curso identificado por _ID
+  *
+  * Requirements:
+  * - O curso deve ter um ID valido.
+  */
+  function changeCourse(uint16 _ID, string memory _name, string memory _academic_degree) public onlyAdmin{
+    require(_ID <= last_course_ID, "course_ID_not_exists");
+
+    courses[_ID].name = _name;
+    courses[_ID].academic_degree = _academic_degree;
+  }
   
+  /*
+  * Adiciona um novo endereco a lista de enderecos autorizados a emitir diplomas.
+  */
   function addIssuer(address _issuer) public onlyAdmin{
     if(!hasIssuer(_issuer))
       issuers.push(_issuer);
   }
 
+  /*
+  * Deleta um endereco da lista de enderecos autorizados a emitir diplomas.
+  *
+  * Requirements:
+  * - O endereco deve existir na lista de enderecos autorizados a emitir diplomas.
+  */
   function removeIssuer(address _issuer) public onlyAdmin{
     int indexToBeDeleted = -1;
     
@@ -82,46 +121,45 @@ contract EDI{
     issuers.pop(); 
   }
 
-  function getIssuers() public view returns(address[] memory){
-    return issuers;
-  }
+  /*
+  * Gera um degreeID a partir do CPF e o nome do curso do estudante.
+  * Requirements:
+  * - O curso deve existir.
+  */
+  function _generateDegreeID(string memory _student_CPF, uint16 _course_ID) internal view returns(bytes32 ){
+    require(hasCourse(_course_ID), "course_not_found");
 
-  function addCourse(string memory _course) public onlyAdmin{
-    if(!hasCourse(_course))
-      IES_courses.push(_course);
+    return keccak256(abi.encodePacked(_student_CPF, courses[_course_ID].name));
   }
   
-  function getCourses() public view returns(string[] memory){
-    return IES_courses;
-  }
-
-  function _generateID(string memory _student_CPF, string memory _course) internal pure returns(bytes32 ){
-    return keccak256(abi.encodePacked(_student_CPF, _course));
-  }
-  
+  /*
+  * Cria um novo diploma.
+  *
+  * Requirements:
+  * - O curso selecionado deve existir;
+  * - O diploma nao pode existir na lista de diplomas.
+  */
   function createDegree(
     string memory _student_CPF,
-    string memory _course,
+    uint16 _course_ID,
     string memory _number,
     string memory _register_number,
     string memory _process_number,
-    string memory _academic_degree,
     uint256 _completion_date,
     uint256 _graduation_date,
     uint256 _registration_date
   ) public onlyIssuers returns(bytes32){
-          
-    bytes32 ID = _generateID(_student_CPF, _course);
+         
+    bytes32 ID = _generateDegreeID(_student_CPF, _course_ID);
 
-    require(hasCourse(_course), "course_not_found");
+    require(hasCourse(_course_ID), "course_not_found");
     require(!hasDegree(ID), "degree_already_exists");
     
     degrees[ID].ID                    = ID;
+    degrees[ID].course_ID             = _course_ID;
     degrees[ID].number                = _number;
     degrees[ID].register_number       = _register_number;
     degrees[ID].process_number        = _process_number;
-    degrees[ID].course                = _course;
-    degrees[ID].academic_degree       = _academic_degree;
     degrees[ID].completion_date       = _completion_date;
     degrees[ID].graduation_date       = _graduation_date;
     degrees[ID].registration_date     = _registration_date;
@@ -130,9 +168,16 @@ contract EDI{
     return ID;     
   }
 
+  /*
+  * Adiciona o estudante ao diploma.
+  *
+  * Requirements:
+  * - O diploma deve ter sido criado anteriomente;
+  * - O diploma nao pode ter sido emitido.
+  */
   function addStudent(
     string memory _CPF,
-    string memory _degree_course,
+    uint16 _course_ID,
     string memory _name,
     string memory _nationality,
     string memory _RG_number,
@@ -142,10 +187,10 @@ contract EDI{
     uint256 _birthdate
   ) public onlyIssuers returns (bytes32){
 
-    bytes32 ID = _generateID(_CPF, _degree_course);
+    bytes32 ID = _generateDegreeID(_CPF, _course_ID);
 
-    require(degrees[ID].issue_date == 0, 'degree_already_issued');
     require(degrees[ID].graduation_date > 0, 'degree_not_created');
+    require(degrees[ID].issue_date == 0, 'degree_already_issued');
 
     degrees[ID].student.name          = _name;
     degrees[ID].student.nationality   = _nationality;
@@ -160,24 +205,26 @@ contract EDI{
     return ID;    
   }
 
-  
   function invalidateDegree(bytes32 _ID) public onlyIssuers{
     degrees[_ID].is_valid = false;
   }
   
-  function getDegreeByID(bytes32 _ID) public view returns (Degree memory){
-    return degrees[_ID];
-  }
-  
-  function getDegreeByCPFAndCourse(string memory _student_CPF, string memory _course) public view returns (Degree memory){
-    bytes32 ID = _generateID(_student_CPF, _course);
+  function getDegreeByCPFAndCourse(string memory _student_CPF, uint16 _course_ID) public view returns (Degree memory){
+    bytes32 ID = _generateDegreeID(_student_CPF, _course_ID);
     return degrees[ID];
   }
-  
-  function hasCourse(string memory _course) public view returns(bool){
-    for (uint16 i=0; i < IES_courses.length; i++) 
-      if (keccak256(abi.encodePacked(IES_courses[i])) == keccak256(abi.encodePacked(_course)))
-        return true;
+
+  function getIssuers() public view returns(address[] memory){
+    return issuers;
+  }
+
+  function getDegreeIDs() public onlyAdmin view returns(bytes32[] memory){
+    return degrees_keys;
+  }
+
+  function hasCourse(uint16 _ID) public view returns(bool){
+    if(_ID <= last_course_ID)
+      return true;
     
     return false;
   }
@@ -196,9 +243,5 @@ contract EDI{
         return true;
     
     return false;
-  }
-
-  function getDegreeIDs() public onlyAdmin view returns(bytes32[] memory){
-    return degrees_keys;
   }
 }

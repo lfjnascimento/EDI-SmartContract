515bb2976c7f8c2fdf66f9d8c879117e3ea53eef
lfjnascimento <lfjnascimento@gmail.com>
2021-11-11 23:29:12 -0400

    Remove comments and change contract name

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,25 +1,15 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
+
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const EDI = await hre.ethers.getContractFactory("EDI");
+  const edi = await EDI.deploy();
 
-  // We get the contract to deploy
-  const Diploma = await hre.ethers.getContractFactory("Diploma");
-  const diploma = await Diploma.deploy();
+  await edi.deployed();
 
-  await diploma.deployed();
+  console.log("EDI deployed to:", edi.address);
+  console.log('Factopry', EDI)
 
-  console.log("Diploma deployed to:", diploma.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
